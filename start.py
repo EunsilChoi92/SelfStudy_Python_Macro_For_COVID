7defb6eb4a14c2e681a5c820df60364f35a02273
EunsilChoi <atheung1@naver.com>
2022-02-02 02:00:54 +0900

    자주 쓰는 코드는 함수로 만듦, 완성!

--- start.py
+++ start.py
@@ -4,28 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 import time
 
-driver = webdriver.Chrome('D:/temp/chromedriver.exe')
-
-url = 'https://hcms.mohw.go.kr/'
-driver.get(url)
-time.sleep(0.5)
-
-# ID 입력
-id_path = "//input[@id='id']"
-id_input = driver.find_element(By.XPATH, id_path)
-id_input.send_keys("아이디")
-
-# Password 입력
-pw_path = "//input[@id='password']"
-pw_input = driver.find_element(By.XPATH, pw_path)
-pw_input.send_keys("비번")
-time.sleep(0.5)
-
-# 로그인
-login_path = "//button[@id='submitBtn']"
-login_btn = driver.find_element(By.XPATH, login_path)
-login_btn.click()
-time.sleep(1)
 
 # 어떤 요소가 존재하는지 확인하기 위한 함수
 def check_exists_by_xpath(xpath):
@@ -34,6 +12,43 @@ def check_exists_by_xpath(xpath):
     except NoSuchElementException:
         return False
     return True
+
+
+# xpath로 요소 찾아서 텍스트 입력하는 함수
+def xpath_send_keys(xpath, keys):
+    element = driver.find_element(By.XPATH, xpath)
+    element.send_keys(keys)
+
+
+# xpath로 요소 찾아서 클릭하는 함수
+def xpath_click(xpath):
+    element = driver.find_element(By.XPATH, xpath)
+    element.click()
+
+
+
+driver = webdriver.Chrome('D:/temp/chromedriver.exe')
+
+url = 'https://hcms.mohw.go.kr/'
+driver.get(url)
+time.sleep(0.5)
+
+
+# ID 입력
+xpath_send_keys("//input[@id='id']", "아이디")
+
+
+# Password 입력
+xpath_send_keys("//input[@id='password']", "비밀번호")
+time.sleep(0.5)
+
+
+# 로그인
+xpath_click("//button[@id='submitBtn']")
+time.sleep(1)
+
+
+
 
 
 '''
@@ -49,47 +64,60 @@ while check_exists_by_xpath("//label[@id='check-notice']"):
 
 
         time.sleep(0.5)
-
 '''
+
 
 # 환자 리스트로 이동
 url = 'https://hcms.mohw.go.kr/clinic/state'
 driver.get(url)
-
 time.sleep(1)
 
+
+# 환자 리스트 개수 전체로 바꾸기
 show_list_xpath = "//select[@id='viewEntry']"
 show_list = Select(driver.find_element(By.ID, "viewEntry"))
 show_list.select_by_visible_text("전체")
-
 time.sleep(1)
+
+
+# 환자 리스트 모두 가져오기
 patient_list_xpath = "//div[@class='patients-stats ']"
 patient_list = driver.find_elements(By.XPATH, patient_list_xpath)
 
 
-# dfiver.get(url)이 한 번만 돌고 자꾸 에러가 남...ㅠㅠ
+# 환자 리스트에서 환자번호 추출해서 각 환자의 개인차트 url을 리스트에 저장
+patient_url_list = []
+
 for element in patient_list:
     patient_url = element.get_attribute("data-url")[1:]
-    print(patient_url)
-    url = "https://hcms.mohw.go.kr/clinic" + patient_url +"&refererPage=1#medicalMemoSection"
+    final_url = "https://hcms.mohw.go.kr/clinic" + patient_url +"&refererPage=1#medicalMemoSection"
+    patient_url_list.append(final_url)
+
+
+# 리스트에 저장된 url로 이동 후 차팅
+cnt = 0     # 카운트(나중에 재원 환자 수와 맞는지 확인할 것)
+for url in patient_url_list:
     driver.get(url)
 
-    # charting_area_xpath = "//textarea[@id='memoContent']"
-    # charting_area = driver.find_element(By.XPATH, charting_area_xpath)
-    # charting_area.send_keys("헤헤헤")
-    #
-    # time_input_xpath = "//input[@id='eventDateTime3']"
-    # time_input = driver.find_element(By.XPATH, time_input_xpath)
-    # time_input.send_keys("2022-02-02 19:00")
-    #
-    # time_btn_xpath = "//button[@id='medicalMemo']"
-    # time_btn = driver.find_element(By.XPATH, time_btn_xpath)
-    # time_btn.click()
+    # 차팅 내용 입력
+    charting = "차팅내용"
+    charting_area_xpath = "//textarea[@id='memoContent']"
+    charting_area = driver.find_element(By.XPATH, charting_area_xpath)
+    charting_area.send_keys(charting)
 
+    # 차팅 시간
+    charting_time = "2022-02-02 19:00"  # ex) 2022-02-02 19:00
+    time_input_xpath = "//input[@id='eventDateTime3']"
+    time_input = driver.find_element(By.XPATH, time_input_xpath)
+    time_input.send_keys(charting_time)
 
+    # 차팅 저장 -------주의!!!!!!!_----------
+    # xpath_click("//button[@id='medicalMemo']")
 
+    # 카운트
+    cnt += 1
 
+    time.sleep(2)
 
+print("차팅 횟수 : %d", cnt)
 
-
-#./info?patientIdx=512047&refererPage=1#medicalMemoSection
